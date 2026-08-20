84af95cf7d57a175a4f0670ac103c1b609c799d8
Nancy Gupta <nancygupta884@gmail.com>
2026-08-20 17:16:55 +0530

    feat: create a get api

--- server.js
+++ server.js
@@ -1,17 +1,31 @@
 const express = require("express");
+const fs = require("fs");
 
 const app = express();
 const PORT = 3000;
 
-// Parse incoming JSON requests
+
 app.use(express.json());
 
-// Basic health check route to confirm the server is running
 app.get("/", (req, res) => {
   res.json({ message: "Smart Event Ticket Booking API is running" });
 });
 
-// Start the server
+
+app.get("/event-status", async (req, res) => {
+  try {
+    const data = await fs.promises.readFile("event.json", "utf-8");
+    const event = JSON.parse(data);
+
+    res.status(200).json({
+      availableSeats: event.availableSeats,
+      bookings: event.bookings,
+    });
+  } catch (error) {
+    res.status(500).json({ error: "Failed to read event data" });
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
